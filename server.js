1f15b0fd6ef016b31adcfc57467a99014cb1c21e
vanshsharma <vansh1910sharma@gmail.com>
2025-03-08 19:56:53 +0530

    some changes

--- server.js
+++ server.js
@@ -14,10 +14,7 @@ app.set("view engine", "ejs");
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected ✅"))
   .catch((err) => console.log("MongoDB Connection Error ❌", err));
 
